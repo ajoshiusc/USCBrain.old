9ba97a08b62e737272f2b2330f3d741eea6f4c0b
Anand A Joshi <ajoshi@usc.edu>
2017-07-26 00:22:58 -0700

    update

--- src/dfsio.py
+++ src/dfsio.py
@@ -1,28 +1,26 @@
 """ This module implements file reading and writing functions for the dfs format for BrainSuite
-    Also see http://brainsuite.bmap.ucla.edu for the software
+    Also see http://brainsuite.org for the software
 """
 
-"""Copyright (C) Brandon Ayers, Shantanu H. Joshi, David Shattuck,
-Brain Mapping Center, University of California Los Angeles
+"""BrainSuite Statistics Toolbox (bss)
+Copyright (C) 2017 The Regents of the University of California
+Creator: Shantanu H. Joshi, Department of Neurology, Ahmanson Lovelace Brain Mapping Center, UCLA
 
-Bss is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
+This program is free software; you can redistribute it and/or modify it under the terms
+of the GNU General Public License as published by the Free Software Foundation; version 2.
 
-Bss is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License version 2 for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA."""
+You should have received a copy of the GNU General Public License along with this program;
+if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA."""
 
-__author__ = "Brandon Ayers"
-__copyright__ = "Copyright 2013, Brandon Ayers, Ahmanson-Lovelace Brain Mapping Center, \
-                 University of California Los Angeles"
-__email__ = "ayersb@ucla.edu"
+
+__author__ = "Brandon Ayers, Anand A Joshi"
+__copyright__ = "Copyright (C) 2017 The Regents of the University of California"
+__maintainer__ = "Shantanu H. Joshi"
+__email__ = "shjoshi@ieee.org"
 
 import numpy as np
 import struct
