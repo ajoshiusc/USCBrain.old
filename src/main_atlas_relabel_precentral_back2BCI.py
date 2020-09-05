f06e87ea8a9f06a4e1d5778157c8ede05cdb4c9d
Anand A Joshi <ajoshi@masal.usc.edu>
2020-09-05 13:53:13 -0700

    update

--- src/main_atlas_relabel_precentral_back2BCI.py
+++ src/main_atlas_relabel_precentral_back2BCI.py
@@ -29,6 +29,11 @@ r2_vert = (right_inner.vertices + right_mid.vertices)/2.0
 l1_vert = (left_pial.vertices + left_mid.vertices)/2.0
 l2_vert = (left_inner.vertices + left_mid.vertices)/2.0
 
+
+vol_lab_new = ni.load_img(USCBrainbase+'/BCI-DNI_brain.label.nii.gz')
+vol_img_new = vol_lab_new.get_fdata()
+
+
 vol_lab = ni.load_img(BCIbase+'/BCI-DNI_brain.label.nii.gz')
 vol_img = vol_lab.get_fdata()
 
@@ -42,8 +47,8 @@ X, Y, Z = np.meshgrid(np.arange(vol_lab.shape[0]), np.arange(vol_lab.shape[1]),
 X = X*xres
 Y = Y*yres
 Z = Z*zres
-#vol_img = sp.mod(vol_img, 1000)
-ind = (vol_img == 150) | (vol_img == 151)  # selects precentral from BCI atlas
+
+ind = (vol_img == 150)  # selects right precentral from BCI atlas
 Xc = X[ind]
 Yc = Y[ind]
 Zc = Z[ind]
@@ -69,15 +74,61 @@ f.labels = np.concatenate((left_mid.labels, right_mid.labels,
                            left_mid.labels, right_mid.labels,
                            left_mid.labels, right_mid.labels))
 
+# selects precentral from uscbrain atlas
+surf_ind = (f.labels == 144) | (f.labels == 146)
+
+f.vertices = f.vertices[surf_ind]
+f.labels = f.labels[surf_ind]
+
+
 t = interpolate_labels(fromsurf=f, tosurf=t)
 
 
-vol_lab = ni.load_img(USCBrainbase+'/BCI-DNI_brain.label.nii.gz')
-vol_img = vol_lab.get_fdata()
+# here make sure that hanns labels are not modified TBD
+vol_img_new[ind] = t.labels
+
+del t, f
+# Left Precentral
+ind = (vol_img == 151)  # selects left precentral from BCI atlas
+Xc = X[ind]
+Yc = Y[ind]
+Zc = Z[ind]
+
+
+class t:
+    pass
+
+
+class f:
+    pass
+
+
+t.vertices = np.concatenate((Xc[:, None], Yc[:, None], Zc[:, None]), axis=1)
+f.vertices = np.concatenate((left_mid.vertices, right_mid.vertices,
+                             left_inner.vertices, right_inner.vertices,
+                             left_pial.vertices, right_pial.vertices,
+                             l1_vert, r1_vert, l2_vert, r2_vert))
+
+f.labels = np.concatenate((left_mid.labels, right_mid.labels,
+                           left_mid.labels, right_mid.labels,
+                           left_mid.labels, right_mid.labels,
+                           left_mid.labels, right_mid.labels,
+                           left_mid.labels, right_mid.labels))
+
+# selects precentral from uscbrain atlas
+surf_ind = (f.labels == 145) | (f.labels == 147)
+
+f.vertices = f.vertices[surf_ind]
+f.labels = f.labels[surf_ind]
+
+
+t = interpolate_labels(fromsurf=f, tosurf=t)
+
 
 # here make sure that hanns labels are not modified TBD
-vol_img[ind] = t.labels
+vol_img_new[ind] = t.labels
 
-new_img = ni.new_img_like(vol_lab, vol_img)
+
+new_img = ni.new_img_like(vol_lab_new, vol_img_new)
 new_img.to_filename(USCBrainbaseLatest +
-                    '/BCI-DNI_brain.label.precent_corr.nii.gz')
+                    '/BCI-DNI_brain.label.nii.gz')
