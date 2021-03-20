12714cce2ba7ef31e1bcb8cbedecd80c0c2a5675
Anand A Joshi <ajoshi@masal.usc.edu>
2021-03-20 00:41:15 -0700

    update

--- src/main_Glasser2016_vol_bci.py
+++ src/main_Glasser2016_vol_bci.py
@@ -6,11 +6,12 @@
 from fmri_methods_sipi import interpolate_labels
 from dfsio import readdfs, writedfs
 import time
+import numpy as np
 import scipy as sp
 import nibabel as nib
 from nilearn import image
 
-subbasename = 'BCI_DNI_Glasser'
+subbasename = 'BCI-DNI_Glasser2016'
 
 left_mid = readdfs(subbasename + '.left.mid.cortex.dfs')
 right_mid = readdfs(subbasename + '.right.mid.cortex.dfs')
@@ -24,21 +25,23 @@ r2_vert = (right_inner.vertices + right_mid.vertices)/2.0
 l1_vert = (left_pial.vertices + left_mid.vertices)/2.0
 l2_vert = (left_inner.vertices + left_mid.vertices)/2.0
 
-vol_lab = image.load_img('/home/ajoshi/BrainSuite19b/svreg/BCI-DNI_brain_atlas/BCI-DNI_brain.label.nii.gz')
-vol_img = vol_lab.get_data()
+vol_lab = image.load_img(
+    '/home/ajoshi/BrainSuite19b/svreg/BCI-DNI_brain_atlas/BCI-DNI_brain.dws.label.nii.gz')
+vol_img = vol_lab.get_fdata()
 
 xres = vol_lab.header['pixdim'][1]
 yres = vol_lab.header['pixdim'][2]
 zres = vol_lab.header['pixdim'][3]
 
-X, Y, Z = sp.meshgrid(sp.arange(vol_lab.shape[0]), sp.arange(vol_lab.shape[1]),
-                      sp.arange(vol_lab.shape[2]), indexing='ij')
+X, Y, Z = np.meshgrid(np.arange(vol_lab.shape[0]), np.arange(vol_lab.shape[1]),
+                      np.arange(vol_lab.shape[2]), indexing='ij')
 
 X = X*xres
 Y = Y*yres
 Z = Z*zres
 #vol_img = sp.mod(vol_img, 1000)
-ind = (sp.floor((vol_img/10)) == 150) | (sp.floor((vol_img/10)) == 151)
+# (np.floor((vol_img/10)) == 150) | (np.floor((vol_img/10)) == 151)
+ind = (vol_img >= 1000) & (vol_img != 2000)
 Xc = X[ind]
 Yc = Y[ind]
 Zc = Z[ind]
@@ -53,21 +56,22 @@ class f:
     pass
 
 
-t.vertices = sp.concatenate((Xc[:, None], Yc[:, None], Zc[:, None]), axis=1)
-f.vertices = sp.concatenate((left_mid.vertices, right_mid.vertices,
+t.vertices = np.concatenate((Xc[:, None], Yc[:, None], Zc[:, None]), axis=1)
+f.vertices = np.concatenate((left_mid.vertices, right_mid.vertices,
                              left_inner.vertices, right_inner.vertices,
                              left_pial.vertices, right_pial.vertices,
                              l1_vert, r1_vert, l2_vert, r2_vert))
 
-f.labels = sp.concatenate((left_mid.labels, right_mid.labels,
+f.labels = np.concatenate((left_mid.labels, right_mid.labels,
                            left_mid.labels, right_mid.labels,
                            left_mid.labels, right_mid.labels,
                            left_mid.labels, right_mid.labels,
                            left_mid.labels, right_mid.labels))
 
-t = interpolate_labels(fromsurf=f,tosurf=t)
+t = interpolate_labels(fromsurf=f, tosurf=t)
 
 # here make sure that hanns labels are not modified TBD
+vol_img = vol_img * 0
 vol_img[ind] = t.labels
 
 new_img = image.new_img_like(vol_lab, vol_img)
