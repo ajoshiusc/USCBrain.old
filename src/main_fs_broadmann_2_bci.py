da98af3e2bbff2cf9b45210dc15c94738fdd2064
Anand A Joshi <ajoshi@masal.usc.edu>
2021-03-25 19:27:42 -0700

    map broadmann areas

--- src/main_fs_broadmann_2_bci.py
+++ src/main_fs_broadmann_2_bci.py
@@ -5,17 +5,13 @@ Created on Tue Aug 16 15:51:16 2016
 @author: ajoshi
 """
 import nibabel.freesurfer.io as fsio
-from surfproc import view_patch
+from surfproc import smooth_patch, view_patch
 from dfsio import writedfs, readdfs
 from nibabel.gifti.giftiio import read as gread
 import os
 from scipy.spatial import cKDTree
 from surfproc import view_patch_vtk, patch_color_labels
-import scipy as sp
-
-p_dir_ref = '/big_disk/ajoshi/HCP_data'
-ref_dir = os.path.join(p_dir_ref, 'reference')
-ref = '100307'
+import numpy as np
 
 
 def interpolate_labels(fromsurf=[], tosurf=[], skipzero=0):
@@ -34,31 +30,42 @@ def interpolate_labels(fromsurf=[], tosurf=[], skipzero=0):
 class bci:
     pass
 
-subbasename = '/big_disk/ajoshi/fs_dir/co20050723_090747MPRAGET1\
-Coronals002a001'
-broadmann = ["BA1", "BA2", "BA3a", "BA3b", "BA4a", "BA4p", "BA6", "BA44",
-             "BA45", "V1", "V2", "MT"]
-#'/big_disk/ajoshi/fs_sub/co20050723_110235Flash3Dt1CORONALs002a001'
+
+broadmann = ["BA1", "BA2", "BA3a", "BA3b", "BA4a",
+             "BA4p", "BA6", "BA44", "BA45", "V1", "V2", "MT"]
 hemi = 'left'
 fshemi = 'lh'
 
-''' BCI to FS processed BCI '''
-bci_bsti = readdfs('/big_disk/ajoshi/coding_ground/svreg-matlab/BCI-DNI\
-_brain_atlas_refined/BCI-DNI_brain.' + hemi + '.mid.cortex.dfs')
-bci_bst = readdfs('/big_disk/ajoshi/coding_ground/svreg-matlab/BCI-DNI\
-_brain_atlas_refined/BCI-DNI_brain.' + hemi + '.inner.cortex.dfs')
-bci_bst.vertices[:, 0] -= 96*0.8
-bci_bst.vertices[:, 1] -= 192*0.546875
-bci_bst.vertices[:, 2] -= 192*0.546875
-bci.vertices, bci.faces = fsio.read_geometry('/big_disk/ajoshi/data/BCI_\
-DNI_Atlas/surf/' + fshemi + '.white')
-bci.labels = sp.zeros(bci.vertices.shape[0])
-for i in range(len(broadmann)):
-    labind = fsio.read_label('/big_disk/ajoshi/data/BCI_DNI_Atlas/label/' +
-                             fshemi + '.' + broadmann[i] + '.thresh.label')
-    bci.labels[labind] = i+1
+for hemi in {'left', 'right'}:
+    if hemi == 'left':
+        fshemi = 'lh'
+    else:
+        fshemi = 'rh'
 
-bci_bst = interpolate_labels(bci, bci_bst)
-s = bci_bst
-s = patch_color_labels(s)
-view_patch_vtk(s)
+    outfile = 'BCI-DNI_Broadmann' + '.' + hemi + '.mid.cortex.dfs'
+
+    ''' BCI to FS processed BCI '''
+    bci_bsti = readdfs(
+        '/home/ajoshi/BrainSuite19b/svreg/BCI-DNI_brain_atlas/BCI-DNI_brain.' + hemi + '.mid.cortex.dfs')
+    bci_bst = readdfs(
+        '/home/ajoshi/BrainSuite19b/svreg/BCI-DNI_brain_atlas/BCI-DNI_brain.' + hemi + '.inner.cortex.dfs')
+    bci_bst_mid = readdfs(
+        '/home/ajoshi/BrainSuite19b/svreg/BCI-DNI_brain_atlas/BCI-DNI_brain.' + hemi + '.inner.cortex.dfs')
+
+    bci_bst.vertices[:, 0] -= 96*0.8
+    bci_bst.vertices[:, 1] -= 192*0.546875
+    bci_bst.vertices[:, 2] -= 192*0.546875
+    bci.vertices, bci.faces = fsio.read_geometry(
+        '/big_disk/ajoshi/data/BCI_DNI_Atlas/surf/' + fshemi + '.white')
+    bci.labels = np.zeros(bci.vertices.shape[0])
+    for i in range(len(broadmann)):
+        labind = fsio.read_label('/big_disk/ajoshi/data/BCI_DNI_Atlas/label/' +
+                                 fshemi + '.' + broadmann[i] + '.thresh.label')
+        bci.labels[labind] = i+1
+
+    bci_bst = interpolate_labels(bci, bci_bst)
+    bci_bst_mid.labels = bci_bst.labels
+    bci_bst_mid = smooth_patch(bci_bst_mid, 6000)
+    bci_bst_labels = patch_color_labels(bci_bst_mid)
+    view_patch_vtk(bci_bst_labels)
+    writedfs(outfile, bci_bst_labels)
