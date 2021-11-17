bf274e5e5a985afd3af9d74be953967c6f0492d6
Anand A Joshi <ajoshi@masal.usc.edu>
2021-11-17 15:48:48 -0800

    update

---
+++ src/main_Brainnetome_2_BCI.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 16 15:51:16 2016
+
+@author: ajoshi
+"""
+import matlab.engine
+import nibabel.freesurfer.io as fsio
+from numpy.lib.function_base import interp
+from surfproc import view_patch_vtk, smooth_patch, patch_color_labels
+from dfsio import writedfs, readdfs
+from nibabel.gifti.giftiio import read as gread
+import os
+from scipy.spatial import cKDTree
+import nibabel as nib
+from lxml import etree
+import numpy as np
+from nilearn import image
+from scipy.interpolate import interpn
+
+atlas_name = 'Brainnetome'
+MNIVol = '/ImagePTE1/ajoshi/BrainnetomeAtlas/BN_Atlas_246_1mm.nii.gz'
+
+btvol = image.resample_to_img(
+    MNIVol, '/ImagePTE1/ajoshi/code_farm/icbm152/mni_icbm152_nlin_asym_09c/icbm152_t1.svreg.label.nii.gz', interpolation='nearest')
+btvol.set_data_dtype('int16')
+btvol.to_filename('tmp_mni.nii.gz')
+MNIVol = 'tmp_mni.nii.gz'
+
+BrainSuitePath = '/home/ajoshi/BrainSuite21a'
+
+applymap_exe = os.path.join(BrainSuitePath, 'svreg',
+                            'bin', 'svreg_apply_map.sh')
+map = '/ImagePTE1/ajoshi/code_farm/icbm152/mni_icbm152_nlin_asym_09c/icbm152_t1.svreg.inv.map.nii.gz'
+
+labelhdr = os.path.join(BrainSuitePath, 'svreg',
+                        'BCI-DNI_brain_atlas', 'BCI-DNI_brain.label.nii.gz')
+outvol = 'BCI-Brainnetome.label.nii.gz'
+outmidl = 'MNI152-'+atlas_name+'.left.mid.cortex.dfs'
+outmidr = 'MNI152-'+atlas_name+'.right.mid.cortex.dfs'
+
+
+lmid = os.path.join(
+    '/ImagePTE1/ajoshi/code_farm/icbm152/mni_icbm152_nlin_asym_09c/icbm152_t1.left.mid.cortex.svreg.dfs')
+rmid = os.path.join(
+    '/ImagePTE1/ajoshi/code_farm/icbm152/mni_icbm152_nlin_asym_09c/icbm152_t1.right.mid.cortex.svreg.dfs')
+
+cmd1 = applymap_exe + ' ' + map + ' ' + MNIVol + ' ' + outvol + ' ' + labelhdr
+
+print(cmd1)
+
+os.system(cmd1)
+
+vol_lab = image.load_img(outvol)
+vol_lab = image.new_img_like(vol_lab, np.int16(vol_lab.get_fdata()))
+vol_lab.to_filename(outvol)
+
+vol_lab = image.load_img(MNIVol)
+
+vol_img = vol_lab.get_fdata()
+
+xres = vol_lab.header['pixdim'][1]
+yres = vol_lab.header['pixdim'][2]
+zres = vol_lab.header['pixdim'][3]
+
+sl = readdfs(lmid)
+sr = readdfs(rmid)
+
+xx = np.arange(vol_lab.shape[0])*xres
+yy = np.arange(vol_lab.shape[1])*yres
+zz = np.arange(vol_lab.shape[2])*zres
+
+sl.labels = interpn((xx, yy, zz), vol_img, sl.vertices, method='nearest')
+sr.labels = interpn((xx, yy, zz), vol_img, sr.vertices, method='nearest')
+
+sl = smooth_patch(sl, iterations=3000, relaxation=.5)
+sr = smooth_patch(sr, iterations=3000, relaxation=.5)
+
+patch_color_labels(sl)
+view_patch_vtk(sl)
+patch_color_labels(sr)
+view_patch_vtk(sr)
+writedfs(outmidl, sl)
+writedfs(outmidr, sr)
+
+eng = matlab.engine.start_matlab()
+eng.addpath(eng.genpath('/ImagePTE1/ajoshi/code_farm/svreg/src'))
+eng.addpath(eng.genpath('/ImagePTE1/ajoshi/code_farm/svreg/3rdParty'))
+eng.addpath(eng.genpath('/ImagePTE1/ajoshi/code_farm/svreg/MEX_Files'))
+
+eng.mni152_to_bci(atlas_name, nargout=0)
+eng.exit()
+
+print('done')
